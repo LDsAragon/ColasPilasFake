56491301ca434d04ac1fd2fd1651af20b18619bc
Matias Salomone <matisalomone@hotmail.com>
2022-09-01 14:59:03 -0300

    Agregar archivos de proyecto.

---
+++ Colas.cs
@@ -0,0 +1,56 @@
+﻿using System.Drawing;
+using System.Xml.Linq;
+using System;
+
+internal class Colas<E>
+{
+    private List<E> cola;
+    private int FIRST_ELEMENT = 0;
+    public Colas() { }
+    public Colas(List<E> cola) { this.cola = cola; }
+
+    public void PutElement(E element) { this.cola.Add(element); }
+
+    public void Remove() { this.cola.RemoveAt( FIRST_ELEMENT ); }
+    public E Peek() { return cola[FIRST_ELEMENT]; } //Peek: mira el valor de la cabeza pero sin extraer.
+    public void Clear() { this.cola = new List<E>(); } //Clear: borra todo el contenido de la cola.
+    public bool Contains(E searcher) { return cola.Contains(searcher); } //Contains: indica si un cierto elemento está en la cola.
+    public Type GetTypes() { return cola.GetType(); } //GetType: para saber de que tipo son los elementos almacenados en la cola.
+    public String getCSV() {
+        String aux = "";
+        for (int i = 0; i < cola.Count(); i++)
+        {
+
+            if (i != FIRST_ELEMENT)
+            {
+                aux += "," + cola[i];
+            }
+            else {
+                aux += cola[i];
+            }
+        }
+        return aux; 
+    } //ToString: devuelve el elemento actual convertido en un string.
+    public E[] ToArray() { return cola.ToArray();  } //ToArray.devuelve toda la cola convertida en un array.
+    public E GetEnumerator(int element)  {
+        try
+        {
+            return cola[element];
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine("Element not found, returning first element instead");
+            return cola[FIRST_ELEMENT] ;
+        }
+        
+    } //GetEnumerator: permite usar enumeradores para recorrer la pila.
+    public int Count() { return cola.Count(); } //Count: indica cua ntos elementos contiene.
+    public E GetElement()
+    {
+        E result = this.cola[FIRST_ELEMENT];
+        this.cola.RemoveAt(FIRST_ELEMENT);
+        return result;
+    }
+
+}
---
+++ Main.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.Intrinsics.X86;
+
+
+public class Runer_class
+{
+    private static void Main(string[] args)
+    {
+        // Colas colas = new Colas(new List<String> { "Hola", "Mundo" });
+        
+        List<String> listaPrueba = new List<String> { "Hola", "Mundo" };
+
+        Colas<String> colas = new Colas<String>(listaPrueba);
+
+        colas.PutElement("Chau");
+        colas.Remove();
+        Console.WriteLine("ToStrings " + colas.getCSV());
+
+        Console.WriteLine("GetElement " + colas.GetElement()); 
+
+        Console.WriteLine("Peek " + colas.Peek());
+
+        Console.WriteLine("Contains Chau ? "+ colas.Contains("Chau"));
+
+        Console.WriteLine("GetTypes " + colas.GetTypes());
+
+        Colas<String> bseLIN =  colas;
+        bseLIN.PutElement("Arturo");
+        Console.WriteLine( bseLIN.getCSV() );
+        Console.WriteLine("ToArray ");
+        Console.WriteLine("[");
+        foreach (String i in bseLIN.ToArray())
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("]");
+
+        Console.WriteLine("Count " + colas.Count());
+
+        Console.WriteLine("GetEnumerator " + colas.GetEnumerator(3));
+
+        
+
+
+
+        List<int> listaInt = new List<int> { 666, 42 };
+
+        Colas<int> colaINT = new Colas<int>(listaInt) ;
+
+        Console.WriteLine(colaINT.getCSV() );
+
+        colaINT.PutElement(78);
+
+        Console.WriteLine(colaINT.getCSV());
+
+
+    }
+}
